5ed0e2497d51b35b761918e319a551e3be4af480
Carter Melnychuk <c.melnychuk97@gmail.com>
2021-06-01 18:12:12 -0600

    New homepage

--- js/index.js
+++ js/index.js
@@ -1,16 +1,3 @@
-let socket;
-let cv;
-let ctx;
-const cWIDTH = 1500;
-const cHEIGHT = 800;
-
-function setup() {
-	// Creating canvas
-	cv = createCanvas(cWIDTH, cHEIGHT);
-	cv.class('main_canvas');
-	ctx = cv.elt.getContext('2d');
-	ctx.imageSmoothingEnabled = false;
-
-	// Start the socket connection
-	socket = io();
-}
+document.getElementById('minesweeper').addEventListener('click', () => {
+	location.href = 'sweeper.html';
+});
