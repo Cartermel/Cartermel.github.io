c6aca91efc16889f7c5abb1bc142257773fa110f
Carter Melnychuk <82341302+Cartermel@users.noreply.github.com>
2021-06-04 11:06:43 -0600

    Added 'undo' functionality + brush cursor.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
 let scrollSpeed = 10;
 let cnv;
-let camera;
 
-let inputs = {
-	"KeyW": false,
-	"KeyS": false,
-	"KeyA": false,
-	"KeyD": false
-};
+let stateHistory = [];
+let currentState;
+
+let customCursor = document.getElementById("custom_cursor");
+
+window.onmousedown = (e) => { if (e.which === 2) e.preventDefault(); }
+
+document.onkeydown = () => {
+	changeColor(Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256));
+}
+
 
 function setup() {
 	cnv = createCanvas(1000, 500);
@@ -16,53 +20,57 @@ function setup() {
 	background(255);
 	stroke(0);
 	strokeWeight(50);
+	document.addEventListener("mousemove", e => {
+		offsetX = mouseX + 50;
+		offsetY = mouseY + 50;
+		customCursor.style.transform = "translate(" + offsetX + "px" + "," + offsetY + "px" + ") scale(1)";
+	});
 }
-let x = 0;
 
 function draw() {
 	if (mouseIsPressed) {
 		if (mouseButton === LEFT) {
 			line(mouseX, mouseY, pmouseX, pmouseY);
-
 		}
 		if (mouseButton === CENTER) {
-
+			background(255);
+			translate(mouseX, mouseY)
+			image(currentState, 0, 0);
 		}
 	}
-	translate(mouseX,mouseY);
+}
 
+function keyPressed(e) {
+	//check if ctrl z pressed and revert.
+	if (e.keyCode == 90 && (e.ctrlKey || e.metaKey)) {
+		undoToPrevState();
+	}
+}
+
+function mousePressed() {
+	//save canvas state as soon as mouse pressed
+	if (mouseButton === LEFT) {
+		saveState();
+	}
+}
+
+function mouseReleased() {
+	currentState = get();
+}
+
+
+function undoToPrevState() {
+	background(255);
+	if (stateHistory.length) {
+		//draw last index of array to canvas and remove it with pop()
+		image(stateHistory.pop(), 0, 0)
+	}
+}
+
+function saveState() {
+	stateHistory.push(get());
 }
 
 function changeColor(r, g, b) {
 	stroke(r, g, b);
 }
-
-
-function scroll() {
-	if (inputs["KeyA"]) window.scrollTo(window.scrollX - scrollSpeed, window.scrollY);
-	if (inputs["KeyD"]) window.scrollTo(window.scrollX + scrollSpeed, window.scrollY);;
-	if (inputs["KeyW"]) window.scrollTo(window.scrollX, window.scrollY - scrollSpeed);;
-	if (inputs["KeyS"]) window.scrollTo(window.scrollX, window.scrollY + scrollSpeed);;
-}
-
-window.onkeydown = keyDown;
-window.onkeyup = keyUp;
-window.onmousedown = (e) => { if (e.which === 2) e.preventDefault(); }
-
-document.onkeydown = () => {
-	changeColor(Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256));
-}
-
-function keyDown(event) { if (event.code in inputs) inputs[event.code] = true; }
-function keyUp(event) { if (event.code in inputs) inputs[event.code] = false; }
-
-// Function that returns a Promise for the FPS
-const getFPS = () =>
-	new Promise(resolve =>
-		requestAnimationFrame(t1 =>
-			requestAnimationFrame(t2 => resolve(1000 / (t2 - t1)))
-		)
-	)
-
-// Calling the function to get the FPS
-getFPS().then(fps => setInterval(scroll, 1000 / fps));
